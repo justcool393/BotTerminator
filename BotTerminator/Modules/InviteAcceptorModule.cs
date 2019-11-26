00d6f8718c4e3bd43d7441aee29c5002663fd523
justcool393 <justcool393@gmail.com>
2019-11-25 20:19:49 -0800

    Fix acceptor modules and add a cooldown

--- BotTerminator/Modules/InviteAcceptorModule.cs
+++ BotTerminator/Modules/InviteAcceptorModule.cs
@@ -8,73 +8,10 @@ using System.Threading.Tasks;
 
 namespace BotTerminator.Modules
 {
-	public class InviteAcceptorModule : BotModule
+	public class InviteAcceptorModule : ListingBotModule<Thing>
 	{
-		public InviteAcceptorModule(BotTerminator bot) : base(bot)
+		public InviteAcceptorModule(BotTerminator bot) : base(bot, bot.RedditInstance.User.GetUnreadMessages(-1))
 		{
-		}
-
-		public override async Task RunOnceAsync()
-		{
-			List<PrivateMessage> privateMessages = new List<PrivateMessage>();
-			await RedditInstance.User.GetUnreadMessages(-1).ForEachAsync(unreadMessage =>
-			{
-				if (unreadMessage is PrivateMessage message)
-				{
-					privateMessages.Add(message);
-				}
-			});
-			const String modInviteMsg = "invitation to moderate /r/";
-			foreach (PrivateMessage privateMessage in privateMessages)
-			{
-				bool shouldMarkRead = true;
-
-				if (privateMessage.Subreddit != null && privateMessage.FirstMessageName == null && privateMessage.Subject.StartsWith(modInviteMsg))
-				{
-					String srName = privateMessage.Subject.Substring(privateMessage.Subject.IndexOf(modInviteMsg) + modInviteMsg.Length);
-					if (String.IsNullOrWhiteSpace(srName))
-					{
-						continue; // handle weird edge case where the subject is literally just "invitation to moderate /r/"
-					}
-					try
-					{
-						await(await RedditInstance.GetSubredditAsync(srName, false)).AcceptModeratorInviteAsync();
-						Console.WriteLine("Accepted moderator invite to /r/{0}", srName);
-					}
-					catch (RedditHttpException ex)
-					{
-						// This is the best we can do without an update to the library
-						if (ex.StatusCode == System.Net.HttpStatusCode.Forbidden)
-						{
-							try
-							{
-								await bot.QuarantineOptInAsync(srName);
-								shouldMarkRead = false;
-							}
-							catch (Exception subException)
-							{
-								Console.WriteLine("Failed to opt in to the quarantine (if it exists): {0}", subException.Message);
-							}
-						}
-					}
-					catch (Exception ex)
-					{
-						Console.WriteLine("Failed to accept moderator invite for subreddit /r/{0}: {1}", srName, ex.Message);
-					}
-				}
-				if (shouldMarkRead)
-				{
-					try
-					{
-						await privateMessage.SetAsReadAsync();
-					}
-					catch (Exception ex)
-					{
-						Console.WriteLine("Failed to mark message {0} as read: {1}", privateMessage.FullName, ex.Message);
-					}
-				}
-			}
-			await bot.UpdateSubredditCacheAsync();
 		}
 
 		public override Task SetupAsync()
@@ -84,5 +21,73 @@ namespace BotTerminator.Modules
 		}
 
 		public override Task TeardownAsync() => Task.CompletedTask;
+
+		protected override async Task PostRunItemsAsync(ICollection<Thing> things)
+		{
+			if (things.Count > 0)
+			{
+				await bot.UpdateSubredditCacheAsync();
+			}
+			await Task.Delay(new TimeSpan(0, 0, 30));
+		}
+
+		protected override Boolean PreRunItem(Thing thing)
+		{
+			return thing is PrivateMessage privateMessage && privateMessage.Subreddit != null && privateMessage.FirstMessageName == null;
+		}
+
+		protected override async Task RunItemAsync(Thing thing)
+		{
+			const String modInviteMsg = "invitation to moderate /r/";
+			PrivateMessage privateMessage = (PrivateMessage)thing;
+			bool shouldMarkRead = true;
+
+			if (privateMessage.Subject.StartsWith(modInviteMsg) && privateMessage.Subject.Length > modInviteMsg.Length)
+			{
+				String srName = privateMessage.Subject.Substring(privateMessage.Subject.IndexOf(modInviteMsg) + modInviteMsg.Length);
+				if (String.IsNullOrWhiteSpace(srName))
+				{
+					await privateMessage.SetAsReadAsync();
+					return; // handle weird edge case where the subject is literally just "invitation to moderate /r/"
+				}
+				try
+				{
+					await (await RedditInstance.GetSubredditAsync(srName, false)).AcceptModeratorInviteAsync();
+					Console.WriteLine("Accepted moderator invite to /r/{0}", srName);
+				}
+				catch (RedditHttpException ex)
+				{
+					// This is the best we can do without an update to the library
+					if (ex.StatusCode == System.Net.HttpStatusCode.Forbidden)
+					{
+						try
+						{
+							await bot.QuarantineOptInAsync(srName);
+							Console.WriteLine("Opted in to the quarantine for subreddit /r/{0}", srName);
+							shouldMarkRead = false;
+						}
+						catch (Exception subException)
+						{
+							Console.WriteLine("Failed to opt in to the quarantine (if it exists): {0}", subException.Message);
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Failed to accept moderator invite for subreddit /r/{0}: {1}", srName, ex.Message);
+				}
+			}
+			if (shouldMarkRead)
+			{
+				try
+				{
+					await privateMessage.SetAsReadAsync();
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Failed to mark message {0} as read: {1}", privateMessage.FullName, ex.Message);
+				}
+			}
+		}
 	}
 }
