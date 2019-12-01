bd8d5bd0bd70517386755a2088ac5f96b137ed4c
justcool393 <justcool393@gmail.com>
2019-11-30 19:57:17 -0800

    Add support for moderator adds (fixes #22) and demods (fixes #28)

--- src/Modules/InviteAcceptorModule.cs
+++ src/Modules/InviteAcceptorModule.cs
@@ -4,14 +4,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BotTerminator.Modules
 {
 	public class InviteAcceptorModule : ListingBotModule<Thing>
 	{
+		private const String moderatorAddSubject = "you are a moderator";
+		/// <summary>
+		/// A regex for the text of a moderator addition.
+		/// </summary>
+		/// <remarks>
+		/// This regex includes support for the time subreddits (e.g. /r/t:1337)
+		/// and subreddits that have periods in their names (e.g. /r/reddit.com).
+		/// </remarks>
+		private static readonly Regex moderatorAddRegex = new Regex(@"^you have been added as a moderator to \[/r/((?:t:)?\w_-\.)+:.*\]\(/r/.+\)\.$", RegexOptions.Compiled);
+		private const String moderatorRemoval = " has been removed as a moderator from /r/";
+
 		public InviteAcceptorModule(BotTerminator bot) : base(bot, bot.RedditInstance.User.GetUnreadMessages(-1))
 		{
+			
 		}
 
 		public override Task SetupAsync()
@@ -28,12 +41,12 @@ namespace BotTerminator.Modules
 			{
 				await bot.UpdateSubredditCacheAsync();
 			}
-			await Task.Delay(new TimeSpan(0, 0, 30));
 		}
 
 		protected override Boolean PreRunItem(Thing message)
 		{
-			return message is PrivateMessage privateMessage && privateMessage.Subreddit != null && privateMessage.FirstMessageName == null;
+			return message is PrivateMessage privateMessage && privateMessage.FirstMessageName == null &&
+			       (privateMessage.Subreddit != null || privateMessage.Subject == moderatorAddSubject);
 		}
 
 		protected override async Task RunItemAsync(Thing messageAsThing)
@@ -42,40 +55,41 @@ namespace BotTerminator.Modules
 			PrivateMessage privateMessage = (PrivateMessage)messageAsThing;
 			bool shouldMarkRead = true;
 
-			if (privateMessage.Subject.StartsWith(modInviteMsg) && privateMessage.Subject.Length > modInviteMsg.Length)
+			if (privateMessage.Subreddit != null && privateMessage.Subject.StartsWith(modInviteMsg) && 
+				privateMessage.Subject.Length > modInviteMsg.Length)
 			{
-				String srName = privateMessage.Subject.Substring(privateMessage.Subject.IndexOf(modInviteMsg) + modInviteMsg.Length);
-				if (String.IsNullOrWhiteSpace(srName))
-				{
-					await privateMessage.SetAsReadAsync();
-					return; // handle weird edge case where the subject is literally just "invitation to moderate /r/"
-				}
+				String subredditName = privateMessage.Subreddit;
 				try
 				{
-					await (await RedditInstance.GetSubredditAsync(srName, false)).AcceptModeratorInviteAsync();
-					Console.WriteLine("Accepted moderator invite to /r/{0}", srName);
+					await (await RedditInstance.GetSubredditAsync(subredditName, false)).AcceptModeratorInviteAsync();
+					Console.WriteLine("Accepted moderator invite to /r/{0}", subredditName);
 				}
 				catch (RedditHttpException ex)
 				{
-					// This is the best we can do without an update to the library
-					if (ex.StatusCode == System.Net.HttpStatusCode.Forbidden)
-					{
-						try
-						{
-							await bot.QuarantineOptInAsync(srName);
-							Console.WriteLine("Opted in to the quarantine for subreddit /r/{0}", srName);
-							shouldMarkRead = false;
-						}
-						catch (Exception subException)
-						{
-							Console.WriteLine("Failed to opt in to the quarantine (if it exists): {0}", subException.Message);
-						}
-					}
+					shouldMarkRead = !await AcceptQuarantineIfValidException(subredditName, ex);
 				}
 				catch (Exception ex)
 				{
-					Console.WriteLine("Failed to accept moderator invite for subreddit /r/{0}: {1}", srName, ex.Message);
+					Console.WriteLine("Failed to accept moderator invite for subreddit /r/{0}: {1}", subredditName, ex.Message);
 				}
+			}
+			else if (privateMessage.Subject == moderatorAddSubject) // these messages aren't sent as a subreddit
+			{
+				Match moderatorAddMatch = moderatorAddRegex.Match(privateMessage.Body);
+				if (moderatorAddMatch != null && moderatorAddMatch.Groups.Count >= 2)
+				{
+					String subredditName = moderatorAddMatch.Groups[1].Value;
+					try
+					{
+						Subreddit subreddit = await RedditInstance.GetSubredditAsync(subredditName, false);
+					}
+					catch (RedditHttpException ex)
+					{
+						// it is possible although rare that we could be added as a moderator to a subreddit that's quarantined
+						await AcceptQuarantineIfValidException(subredditName, ex);
+					}
+				}
+
 			}
 			if (shouldMarkRead)
 			{
@@ -88,6 +102,25 @@ namespace BotTerminator.Modules
 					Console.WriteLine("Failed to mark message {0} as read: {1}", privateMessage.FullName, ex.Message);
 				}
 			}
+		}
+
+		private async Task<bool> AcceptQuarantineIfValidException(String subredditName, RedditHttpException exception)
+		{
+			// This is the best we can do without an update to the library
+			if (exception.StatusCode == System.Net.HttpStatusCode.Forbidden)
+			{
+				try
+				{
+					await bot.QuarantineOptInAsync(subredditName);
+					Console.WriteLine("Opted in to the quarantine for subreddit /r/{0}", subredditName);
+					return true;
+				}
+				catch (Exception subException)
+				{
+					Console.WriteLine("Failed to opt in to the quarantine (if it exists): {0}", subException.Message);
+				}
+			}
+			return false;
 		}
 	}
 }
