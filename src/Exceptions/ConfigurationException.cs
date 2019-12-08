2ca1076c8631bdda4a2791f43bfa838bc2cffe2e
justcool393 <justcool393@gmail.com>
2019-12-08 12:01:23 -0800

    Add exceptions and don't fail on a bad config

---
+++ src/Exceptions/ConfigurationException.cs
@@ -0,0 +1,29 @@
+﻿using BotTerminator.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BotTerminator.Exceptions
+{
+	public class ConfigurationException : BotTerminatorException
+	{
+		private const String baseExceptionMessage = "The configuration that was loaded is invalid";
+
+		public ConfigurationException() : this(baseExceptionMessage)
+		{
+		}
+
+		public ConfigurationException(String message) : base(message)
+		{
+		}
+
+		public ConfigurationException(String message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		protected ConfigurationException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
