359045bd0547030ebae033e0291d643c94806aa8
justcool393 <justcool393@gmail.com>
2019-12-09 20:59:24 -0800

    Add and enable removed post module

---
+++ src/Modules/RemovedPostScannerModule.cs
@@ -0,0 +1,21 @@
+﻿using RedditSharp;
+using RedditSharp.Things;
+using System;
+using System.Threading.Tasks;
+
+namespace BotTerminator.Modules
+{
+	public class RemovedPostScannerModule : ScannerModule<Post>
+	{
+		public RemovedPostScannerModule(BotTerminator bot) : base(bot, null, bot.RedditInstance.GetListing<Post>(BotTerminator.ModRemovedUrl, 100, 100))
+		{
+			RunForeverCooldown = new TimeSpan(0, 0, 10);
+		}
+
+		public override Task SetupAsync()
+		{
+			Console.WriteLine("Starting removed post scanner module");
+			return Task.CompletedTask;
+		}
+	}
+}
