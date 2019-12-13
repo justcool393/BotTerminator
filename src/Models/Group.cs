bc912f00d4a604d0a3408e4077e37b92425bc064
justcool393 <justcool393@gmail.com>
2019-12-13 11:02:31 -0800

    Create Group and GroupSet class

---
+++ src/Models/Group.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotTerminator.Models
+{
+	[JsonObject]
+	public class Group
+	{
+		[JsonProperty("id", Required = Required.Always)]
+		public Guid Id { get; private set; }
+
+		[JsonProperty("name", Required = Required.Always)]
+		public String Name { get; private set; }
+		
+		[JsonProperty("postFlairCssClass", Required = Required.Always)]
+		public String PostFlairCssClass { get; private set; }
+
+		[JsonProperty("members")]
+		public ISet<String> Members { get; private set; } = new HashSet<String>();
+	}
+}
