506f5ad7df07d0b257319b7c5383de1edd6cf026
justcool393 <justcool393@gmail.com>
2019-12-13 11:02:31 -0800

    Create Group and GroupSet class

---
+++ src/Models/GroupSet.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BotTerminator.Models
+{
+	public class GroupSet
+	{
+		[JsonProperty("groups")]
+		public Dictionary<String, Group> GroupLookup { get; set; }
+
+		public bool IsInGroup(String groupCssClass, String username)
+		{
+			return GroupLookup[groupCssClass].Members.Contains(username);
+		}
+
+		public bool IsInAnyGroup(String username)
+		{
+			return GroupLookup.Values.Any(group => group.Members.Contains(username));
+		}
+	}
+}
