50ea7f874721aee6b368b4799cb693a78d02107c
justcool393 <justcool393@gmail.com>
2019-12-15 20:45:45 -0800

    make memory bot database implement cacheable DB and add TTL

--- src/Data/MemoryBotDatabase.cs
+++ src/Data/MemoryBotDatabase.cs
@@ -8,31 +8,10 @@ using BotTerminator.Models;
 
 namespace BotTerminator.Data
 {
-	public class MemoryBotDatabase : IBotDatabase
+	public class MemoryBotDatabase : CacheableBotDatabase
 	{
-		private BanListConfig Users { get; set; } = new BanListConfig();
+		protected override Task UpdateAsync() => Task.CompletedTask;
 
-		public Task<BanListConfig> GetConfigAsync() => Task.FromResult(Users);
-
-		public Task<IReadOnlyDictionary<String, Group>> GetAllGroupsAsync() => Task.FromResult<IReadOnlyDictionary<String, Group>>(Users.GroupLookup);
-
-		public Task<Boolean> CheckUserAsync(String username, String groupName) => Task.FromResult(Users.IsInGroup(groupName, username));
-
-		public Task<IReadOnlyCollection<Group>> GetDefaultBannedGroupsAsync() => Task.FromResult(Users.GroupLookup.Where(group => group.Value.ActionByDefault).ToList() as IReadOnlyCollection<Group>);
-
-		public Task<IReadOnlyCollection<Group>> GetGroupsForUserAsync(String username) => Task.FromResult(Users.GetGroupsByUser(username));
-
-		public Task UpdateUserAsync(String name, String groupName, Boolean value, Boolean force = false)
-		{
-			if (value)
-			{
-				Users.GroupLookup[groupName].Members.Add(name);
-			}
-			else
-			{
-				Users.GroupLookup[groupName].Members.Remove(name);
-			}
-			return Task.CompletedTask;
-		}
+		protected override Task UpdateUserAsync(String username, String groupName, Boolean value) => Task.CompletedTask;
 	}
 }
