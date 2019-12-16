44e8142c3b9c5b756dab85b77dbb4bbdc720c2a0
justcool393 <justcool393@gmail.com>
2019-12-15 20:25:00 -0800

    Create cacheable database

---
+++ src/Data/CacheableDatabase.cs
@@ -0,0 +1,72 @@
+﻿using BotTerminator.Configuration;
+using BotTerminator.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotTerminator.Data
+{
+	public abstract class CacheableDatabase : IBotDatabase
+	{
+		protected BanListConfig Users { get; set; }
+
+		protected DateTimeOffset LastUpdated { get; set; }
+
+		protected TimeSpan Ttl { get; set; } = TimeSpan.MaxValue;
+		public Boolean IsStale => DateTimeOffset.UtcNow - LastUpdated > Ttl;
+
+		protected async Task UpdateIfStaleAsync()
+		{
+			if (IsStale)
+			{
+				await UpdateAsync();
+				LastUpdated = DateTimeOffset.UtcNow;
+			}
+		}
+
+		protected abstract Task UpdateAsync();
+
+		protected abstract Task UpdateUserAsync(String username, String groupName, Boolean value);
+
+		public async Task<BanListConfig> GetConfigAsync()
+		{
+			await UpdateIfStaleAsync();
+			return Users;
+		}
+
+		public async Task<IReadOnlyDictionary<String, Group>> GetAllGroupsAsync()
+		{
+			await UpdateIfStaleAsync();
+			return Users.GroupLookup;
+		}
+
+		public async Task<IReadOnlyCollection<Group>> GetGroupsForUserAsync(String name)
+		{
+			await UpdateIfStaleAsync();
+			return Users.GetGroupsByUser(name);
+		}
+
+		public async Task<IReadOnlyCollection<Group>> GetDefaultBannedGroupsAsync()
+		{
+			await UpdateIfStaleAsync();
+			return Users.GetDefaultActionedOnGroups();
+		}
+
+		public async Task<Boolean> CheckUserAsync(String username, String groupName)
+		{
+			await UpdateIfStaleAsync();
+			return Users.IsInGroup(groupName, username);
+		}
+
+		public async Task UpdateUserAsync(String username, String groupName, Boolean value, Boolean force = false)
+		{
+			if (force || IsStale)
+			{
+				await UpdateUserAsync(username, groupName, value);
+				LastUpdated = DateTimeOffset.UtcNow;
+			}
+			throw new NotImplementedException();
+		}
+	}
+}
