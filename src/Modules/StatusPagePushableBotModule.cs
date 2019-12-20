ac1e098b01f2386924aaba5314d589f0d55076e1
justcool393 <justcool393@gmail.com>
2019-12-20 11:30:51 -0800

    Allow committing of multiple metrics in one request

--- src/Modules/StatusPagePushableBotModule.cs
+++ src/Modules/StatusPagePushableBotModule.cs
@@ -12,8 +12,6 @@ namespace BotTerminator.Modules
 {
 	public abstract class StatusPagePushableBotModule : BotModule
 	{
-		private const String ApiBaseUrl = "https://api.statuspage.io/v1/pages/{0}/metrics/{1}/data.json";
-
 		protected abstract String MetricId { get; }
 
 		protected StatusPagePushableBotModule(BotTerminator bot) : base(bot) 
@@ -31,15 +29,8 @@ namespace BotTerminator.Modules
 			{
 				throw new StatusPagePushException("Cannot push to status page when metric id is null", new ArgumentNullException(nameof(MetricId)));
 			}
-			JObject data = new JObject(new JProperty("data", JObject.FromObject(metric)));
-			bot.StatusPageQueue.Enqueue(() =>
-			{
-				StringContent content = new StringContent(data.ToString(Newtonsoft.Json.Formatting.None), Encoding.UTF8, "application/json");
-				return new HttpRequestMessage(HttpMethod.Post, String.Format(ApiBaseUrl, bot.GlobalConfig.StatusPagePageId, MetricId))
-				{
-					Content = content,
-				};
-			});
+			metric.MetricId = metric.MetricId ?? MetricId;
+			bot.StatusPageQueueData.Enqueue(metric);
 			return Task.CompletedTask;
 		}
 	}
